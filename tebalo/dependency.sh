a7c3837f158ccb4a6138065708912c8f1364f5cf
Vishnu Ks <yo@vishnuks.com>
2016-11-30 01:50:30 +0530

    Add depedency file.

--- tebalo/dependency.sh
+++ tebalo/dependency.sh
@@ -1,4 +1,6 @@
 wget https://bootstrap.pypa.io/get-pip.py 
 sudo apt-get install python-dev           
-sudo -E python get-pip.py         
+sudo -E python get-pip.py
+rm get-pip.py         
 sudo -E pip install psutil 
+sudo apt-get install sensors
