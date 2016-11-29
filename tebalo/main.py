87df50e9e0fa48ab02105a8ab113241a48c5b72a
Sameer Killamsetty <sameerkillamsetty@gmail.com>
2016-11-30 01:39:11 +0530

    added functionality to main

--- tebalo/main.py
+++ tebalo/main.py
@@ -1,1 +1,29 @@
+import os
+import requests
+import time
+import subprocess
+import utils
 
+
+current_loads = utils.get_current_loads()
+max_load = utils.max_load()
+min_load =utils.min_load()
+avail_freqs = utils.get_core_freqeuncies()
+
+def expected_set_frequencies():
+	no_of_avail_freqs = len(avail_freqs)
+	block_size = (max_load - min_load)/no_of_avail_freqs
+	set_freq = []
+	for load in current_loads:
+		temp = min_load
+		for freq in avail_freqs:
+			if(temp <= load  <= temp + block_size) :
+				set_freq.append(freq)
+				break
+			temp += block_size	
+
+	return set_freq		
+
+if __name__ == "__main__":
+	freqs = expected_set_frequencies()
+	
--- tebalo/utils.py
+++ tebalo/utils.py
@@ -1,14 +1,22 @@
 import psutil
 
-def get_current_load():
+def get_current_loads():
 	return psutil.cpu_percent(interval=2, percpu=True)
 
 def get_core_count():
 	return psutil.cpu_count(logical=False)
 
-def get_core_freqeuncies(core_number):
+def get_core_freqeuncies(core_number= 0):
 	frequency_file = open("/sys/devices/system/cpu/cpu%s/cpufreq/scaling_available_frequencies" % str(core_number), "r")
 	lines = frequency_file.read().split(' ')
+	return lines
+
+def max_load():
+	return 100
+
+def min_load() :
+	return 0
+
 
 
 
