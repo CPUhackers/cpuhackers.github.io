ca4b8f4349e85bde316d2b3775ab103481a45c1e
Sameer Killamsetty <sameerkillamsetty@gmail.com>
2016-11-30 03:26:38 +0530

    sensors changed to lm-sensors

--- tebalo/dependency.sh
+++ tebalo/dependency.sh
@@ -3,5 +3,5 @@ sudo apt-get install python-dev
 sudo -E python get-pip.py
 rm get-pip.py         
 sudo -E pip install psutil 
-sudo apt-get install sensors
+sudo apt-get install lm-sensors
 sudo apt-get install cpufrequtils
