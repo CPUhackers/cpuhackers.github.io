2c6e01acfccb17f78da42e3e7363f5c67e5abe26
Vishnu Ks <yo@vishnuks.com>
2016-11-30 01:50:15 +0530

    Add temparature_read function.

---
+++ tebalo/create_core_temp_file.sh
@@ -0,0 +1,2 @@
+sensors | grep "Core 0" > core1.temp
+sensors | grep "Core 1" > core2.temp
--- tebalo/utils.py
+++ tebalo/utils.py
@@ -1,4 +1,5 @@
 import psutil
+import subprocess
 
 def get_current_loads():
 	return psutil.cpu_percent(interval=2, percpu=True)
@@ -6,7 +7,7 @@ def get_current_loads():
 def get_core_count():
 	return psutil.cpu_count(logical=False)
 
-def get_core_freqeuncies(core_number= 0):
+def get_core_freqeuncies(core_number=0):
 	frequency_file = open("/sys/devices/system/cpu/cpu%s/cpufreq/scaling_available_frequencies" % str(core_number), "r")
 	lines = frequency_file.read().split(' ')
 	return lines
@@ -17,6 +18,18 @@ def max_load():
 def min_load() :
 	return 0
 
+def get_core_temparature():
+	temparature_list = []
+	subprocess.call("./create_core_temp_file.sh", shell=True)
+	temp1_file = open("core1.temp")
+	line1 = temp1_file.read()
+	temparature_list.append(line1[17:19])
+
+	subprocess.call("./create_core_temp_file.sh", shell=True)
+	temp2_file = open("core2.temp")
+	line2 = temp2_file.read()
+	temparature_list.append(line2[17:19])
+	return temparature_list
 
 
 
